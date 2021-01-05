fbd00d82a9682ce77defb8122ebedc462f4d1d2b
Jesse <jesseb727@gmail.com>
2021-01-05 21:21:16 +0200

    Add functionality to use ByteCareAPI. Init ByteCareAPI for rest of app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+/* Project-level Imports */
+// Services
+import 'services/bytecare_api.dart';
+
+// Screens
 import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 
 void main() {
+  // Gonna need to change this when debugging, cause of different IPs.
+  Uri apiHostUri = Uri(
+    scheme: 'https',
+    host: '127.0.0.1',
+    port: 5000,
+  );
+
+  ByteCareAPI.initInstance(apiHostUri);
   runApp(ByteCareMobile());
 }
 
