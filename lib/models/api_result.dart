ed1442a9a166b089a46fd99ef72d4b8ff82889ff
Jesse <jesseb727@gmail.com>
2021-01-16 09:12:27 +0200

    Add field for storing data in ApiResult

--- lib/models/api_result.dart
+++ lib/models/api_result.dart
@@ -2,6 +2,7 @@ class ApiResult {
   final int code;
   final String message;
   final bool hasError;
+  final dynamic data;
 
-  ApiResult({this.code, this.message, this.hasError});
+  ApiResult({this.code, this.message, this.hasError, this.data});
 }
