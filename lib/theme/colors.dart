78908d5840aabeeffe96c97f600cafcf67dc216f
Jesse <jesseb727@gmail.com>
2021-02-17 11:07:45 +0200

    Update theme success and warning colors and add warning color.

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 var kThemeColorPrimary = Color(0xFF1D9FCF);
 var kThemeColorSecondary = Color(0xFF940094);
-var kThemeColorSuccess = Color(0xFF30B400);
-var kThemeColorError = Colors.red;
+var kThemeColorSuccess = Color(0xFF55CC00);
+var kThemeColorError = Color(0xFFCC1100);
+var kThemeColorWarning = Color(0xFFC2CC00);
